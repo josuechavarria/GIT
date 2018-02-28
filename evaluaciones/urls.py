a49bc3b17d7f4982942c3f0902f2379a72c4f73c
Josue Carranza <josuechavarria89@gmail.com>
2018-02-28 01:36:45 -0600

    url post login
    url login

--- evaluaciones/urls.py
+++ evaluaciones/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.principal, name='principal'),
+    url(r'^$', views.LoginView.as_view(), name='login'),
     url(r'^empresas/crear', views.CrearEmpresa.as_view(), name='empresa.crear'),
 ]
