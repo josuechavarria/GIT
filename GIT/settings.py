673b1dd4a318f1db02d3e343813c526458e744d9
Josue Carranza <josuechavarria89@gmail.com>
2018-04-08 15:59:50 -0600

    config heroku

--- GIT/settings.py
+++ GIT/settings.py
@@ -133,3 +133,18 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'josuechavarria89@gmail.com' #hancoir@gmail.com
 EMAIL_HOST_PASSWORD = '31829kmx' #******
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage
