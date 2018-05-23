1995d3a2e39acc99174bd1d526b6f6eb8c18c5ec
Josue Carranza <josuechavarria89@gmail.com>
2018-05-23 01:10:42 -0600

    actualizacion
    por problema en rama

--- evaluaciones/urls.py
+++ evaluaciones/urls.py
@@ -88,6 +88,7 @@ urlpatterns = [
     url(r'^evaluaciones/listar_/(?P<pk>\d+)/$',
         views.ListarEvaluaciones.as_view(), name='listar_evaluaciones'),    
     url(r'^evaluaciones/borrar/', views.borrar_evaluaciones.as_view(), name='borrar_evaluaciones'),
+    url(r'^evaluaciones/notificar/refresh/(?P<pk>\d+)/(?P<id>\d+)$', views.NotificacionesRefresh.as_view(), name='notificacion_refresh'),
     url(r'^evaluaciones/actualizar/tabla/', views.actualizar_tabla,name='actualizar_tabla'),    
     url(r'^evaluaciones/actualizar/tablacriterios/', views.actualizar_tablacriterios,name='actualizar_tablacriterios'),
     url(r'^evaluaciones/modificar/(?P<pk>\d+)/$',views.ListarEvaluaciones_modificar.as_view(), name='listar_evaluaciones_modificar'),
